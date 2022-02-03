e26e8689e18cba371ba234ff23f4ddc82766bfc2
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-02-03 09:50:59 -0700

    lots of work, going to test instructions

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+let cons = require('consolidate');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const { json } = require('express/lib/response');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('html', cons.swig)
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'pages'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,48 +26,132 @@ const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     email: String,
-    age: { type: Number, min: 18, max: 70 },
+    age: Number,
     createdDate: { type: Date, default: Date.now }
 
 });
-const user = mongoose.model('User', userSchema);
+const user = mongoose.model('user', userSchema);
 
 app.get('/', (req, res) => {
-    res.render('main');
+    res.render('home');
 })
 
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+//create user
 app.post('/create', (req, res) => {
     console.log(`POST /create: ${JSON.stringify(req.body)}`);
     const newUser = new user();
     newUser.firstName = req.body.firstName;
     newUser.lastName = req.body.lastName;
     newUser.email = req.body.email;
+    newUser.age = req.body.age;
     newUser.save((err, data) => {
         if (err) {
             return console.error(err);
         }
-        console.log(`new user save: ${data}`);
-        res.send(`done ${data}`);
+        let returnMsg = `
+            <h1>User Successfully Created User</h1>
+            <p>First name: ${newUser.firstName}</p>
+            <p>Last name: ${newUser.lastName}</p>
+            <p>Email: ${newUser.email}</p>
+            <p>Age: ${newUser.age}</p>
+            <a href="/">Back</a>
+            `;
+        res.send(returnMsg);
     });
 })
 
-//change to update many
 
-// app.post('/edit', (req, res) => {
-//     console.log(`POST /edit: ${JSON.stringify(req.body)}`);
-//     let matchedName = req.body.firstName;
-//     let newrole = req.body.role;
-//     user.findOneAndUpdate({ name: matchedName }, { role: newrole },
-//         { new: true }, //return the updated version instead of the pre-updated document 
-//         (err, data) => {
-//             if (err) return console.log(`Oops! ${err}`);
-//             console.log(`data -- ${data.role}`)
-//             let returnMsg = `username : ${matchedName} New role : ${data.role}`;
-//             console.log(returnMsg);
-//             res.send(returnMsg);
-//         });
-// })
+//User edit
+app.post('/edit', (req, res) => {
+    console.log(`POST /edit: ${JSON.stringify(req.body)}`);
+    let userToChange = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    let age = req.body.age;
+    user.findOneAndUpdate({ firstName: userToChange }, { lastName: lastName, email: email, age: age },
+        { new: true },
+        (err, data) => {
+            if (err) return console.log(`Oops! ${err}`);
+            let returnMsg = `
+            <h1>User Successfully Edited</h1>
+            <p>First name: ${userToChange}</p>
+            <p>Last name: ${lastName}</p>
+            <p>Email: ${email}</p>
+            <p>Age: ${age}</p>
+            <a href="/">Back</a>
+            `;
+            res.send(returnMsg);
+        });
+})
 
+//delete user
+app.post('/delete', (req, res) => {
+    console.log(`POST /delete: ${JSON.stringify(req.body)}`);
+    let firstToGo = req.body.firstName;
+    let lastToGo = req.body.lastName
+    user.deleteOne({ firstName: firstToGo, lastName: lastToGo },
+        (err, data) => {
+            console.log(data);
+            if (err) return console.log(`Oops! ${err}`);
+            let returnMsg = `
+            <h1>User Successfully Deleted</h1>
+            <p>${firstToGo} ${lastToGo} has been deleted</p>
+            `;
+            if (data.deletedCount == 0) {
+                returnMsg = `<h1>Error</h1><p>${firstToGo} ${lastToGo} does not exists in the database</p>`
+            }
+            returnMsg += '<a href="/">Back</a>'
+            res.send(returnMsg);
+        });
+})
+
+//show users
+app.get('/users', (req, res) => {
+    let returnMsg = '<h1>Users:</h1>'
+    user.find({}, (err, users) => {
+        //console.log(users);
+        users.map((item) => {
+            returnMsg += `
+            <p>First Name: ${item.firstName}</p>
+            <p>Last Name: ${item.lastName}</p>
+            <p>Email: ${item.email}</p>
+            <p>Age: ${item.age}</p>
+            <p>----------------</p>
+            `
+        })
+        returnMsg += '<a href="/">Back</a>'
+        res.send(returnMsg)
+    })
+
+
+})
+
+//search box
+app.post('/search', (req, res) => {
+    let query = req.body.query;
+    user.find({ $or: [{ firstName: query }, { lastName: query }] }, (err, users) => { //{ firstName: query }
+        let returnMsg = '<h1>Results:</h1>'
+        console.log(users);
+        if (users == false) {
+            returnMsg += 'No Results <br><br>'
+        }
+        users.map((item) => {
+            returnMsg += `
+            <p>First Name: ${item.firstName}</p>
+            <p>Last Name: ${item.lastName}</p>
+            <p>Email: ${item.email}</p>
+            <p>Age: ${item.age}</p>
+            <p>----------------</p>
+            `
+        })
+        returnMsg += '<a href="/search">Back</a>'
+        res.send(returnMsg)
+    })
+})
 
 app.listen(port, (err) => {
     if (err) console.log(err);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Instructions
+
+1. Clone Repo
+
+2. In the ```user-management-mongoDB``` folder run ```npm init```
